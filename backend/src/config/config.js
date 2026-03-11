16ad67a753e13125f770421b98905cbf4640026d
jdhernandez12 <desarrolladorjr12@tecsersas.com>
2026-03-11 16:57:43 -0500

    Cambio en variables de entorno

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,25 +1,35 @@
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME || "root",
-    password: process.env.DB_PASSWORD || "yvCcACLLfrIgHvXPxHcakJzeGeKMosLO", // Contraseña de tu base de datos
-    database: process.env.DB_NAME || "railway", // Nombre de la base de datos
-    host: process.env.DB_HOST || "junction.proxy.rlwy.net", // Host de la base de datos
-    port: process.env.DB_PORT || 38840, // Puerto de la base de datos
-    dialect: "postgres"
+    username: process.env.DB_USERNAME || "superalimento_mzwz_user",
+    password: process.env.DB_PASSWORD || "uE3qs2GoFPG42JTWWo52VLcX80vTNPnP",
+    database: process.env.DB_NAME || "superalimento_mzwz",
+    host: process.env.DB_HOST || "dpg-d6otvchr0fns73dvph8g-a.oregon-postgres.render.com",
+    port: process.env.DB_PORT || 5432,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   },
+
   test: {
     username: process.env.DB_USERNAME || "root",
     password: process.env.DB_PASSWORD || null,
-    database: process.env.DB_TEST_NAME || "database_test",
+    database: process.env.DB_TEST_NAME || "superalimento_mzwz_test",
     host: process.env.DB_HOST || "127.0.0.1",
     dialect: "postgres"
   },
+
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 5432,
-    dialect: "postgres"
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 };
--- backend/src/migrations/20240824213110-create-rol-table.js
+++ backend/src/migrations/20240824213110-create-rol-table.js
@@ -15,7 +15,7 @@ module.exports = {
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'),
+        defaultValue: Sequelize.NOW,
       },
       nombre_usuario: {
         type: Sequelize.STRING,
