441083649ecf52fe49be8940f2266b520bbf04ad
jdhernandez1832 <157819208+jdhernandez1832@users.noreply.github.com>
2026-03-11 16:17:17 -0500

    Update config.js

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -5,14 +5,14 @@ module.exports = {
     database: process.env.DB_NAME || "railway", // Nombre de la base de datos
     host: process.env.DB_HOST || "junction.proxy.rlwy.net", // Host de la base de datos
     port: process.env.DB_PORT || 38840, // Puerto de la base de datos
-    dialect: "mysql"
+    dialect: "postgres"
   },
   test: {
     username: process.env.DB_USERNAME || "root",
     password: process.env.DB_PASSWORD || null,
     database: process.env.DB_TEST_NAME || "database_test",
     host: process.env.DB_HOST || "127.0.0.1",
-    dialect: "mysql"
+    dialect: "postgres"
   },
   production: {
     username: process.env.DB_USERNAME,
@@ -20,6 +20,6 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT || 3306,
-    dialect: "mysql"
+    dialect: "postgres"
   }
 };
