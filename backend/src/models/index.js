726a04b3238c4402c5c7e6fece5a3d9e276850ad
jdhernandez12 <desarrolladorjr12@tecsersas.com>
2026-03-11 17:22:05 -0500

    Cambios index.js

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -3,11 +3,21 @@ const fs = require('fs');
 const path = require('path');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.js')[env]; // Importa config.js
+const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
 let sequelize;
-sequelize = new Sequelize(config.database, config.username, config.password, config);
+
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
+}
 
 fs.readdirSync(__dirname)
   .filter(file => {
